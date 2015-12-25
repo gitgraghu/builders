8ad6508486cc18e9b27d05e775aee0243b967269
Govind Raghu <govindkrishna@gmail.com>
2015-12-25 21:48:00 +0530

    wiring main route to index page

---
+++ server.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/builders');
+
+var bodyParser = require('body-parser');
+var winston = require('winston');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+winston.add(
+    winston.transports.File, {
+      filename: 'server.log',
+      level: 'info',
+      json: true,
+      eol: 'n',
+      timestamp: true
+    }
+);
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+       res.sendfile('./public/index.html');
+});
+
+app.use('/', router);
+app.listen(port);
